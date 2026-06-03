cde21401a618bbf3b20a4be0ec9da95a3a463a9e
ishaansogoated <kyloisverycool@gmail.com>
2026-06-03 20:23:40 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
-
 export const CONFIG = {
-  siteName: "The FourWord",
-  googleSheetCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vScwdhjSHbBX7y3_UuR37JdeOsYqQKqU9v_gNVhnsY44PbchbvrSKXARKXagpjB11c--nHYCX6HqV63/pub?gid=0&single=true&output=csv",
-  dictionaryPath: "./data/dictionary.json",
+  siteName: "THE FOURWORD",
+
+  googleSheetCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vScwdhjSHbBX7y3_UuR37JdeOsYqQKqU9v_gNVhnsY44PbchbvrSKXARKXagpjB11c--nHYCX6HqV63/pub?gid=0&single=true&output=csv", 
+  
+  dictionaryPath: "https://ishaansogoated.github.io/TheFourWord/data/dictionary.json",
   
   fallbackPuzzle: {
     date: "2026-06-03",
-    puzzleNumber: 0,
-    seedWord: "WORD"
+    puzzleNumber: 1,
+    seedWord: "GAME" // Using a valid word from your starter dictionary
   }
 };
