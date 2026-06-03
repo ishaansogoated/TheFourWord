1eed86e9192c099a3f4072c4cac556380bf11edf
ishaansogoated <kyloisverycool@gmail.com>
2026-06-03 20:13:19 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,12 @@
 
+export const CONFIG = {
+  siteName: "The FourWord",
+  googleSheetCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vScwdhjSHbBX7y3_UuR37JdeOsYqQKqU9v_gNVhnsY44PbchbvrSKXARKXagpjB11c--nHYCX6HqV63/pub?gid=0&single=true&output=csv",
+  dictionaryPath: "./data/dictionary.json",
+  
+  fallbackPuzzle: {
+    date: "2026-06-03",
+    puzzleNumber: 0,
+    seedWord: "WORD"
+  }
+};
