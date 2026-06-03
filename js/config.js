a7df1d54451d5f7813ca953a395d02be676a4908
ishaansogoated <kyloisverycool@gmail.com>
2026-06-03 21:55:29 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ export const CONFIG = {
 
   googleSheetCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vScwdhjSHbBX7y3_UuR37JdeOsYqQKqU9v_gNVhnsY44PbchbvrSKXARKXagpjB11c--nHYCX6HqV63/pub?gid=0&single=true&output=csv", 
   
-  dictionaryPath: "https://ishaansogoated.github.io/TheFourWord/data/dictionary.json",
+  dictionaryPath: "https://ishaansogoated.github.io/TheFourWord/data/4-letter-words.json",
   
   fallbackPuzzle: {
     date: "2026-06-03",
