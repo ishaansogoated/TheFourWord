64c5fdd82357a964afc4f1c20061276f8f35b2a9
ishaansogoated <kyloisverycool@gmail.com>
2026-06-04 21:09:25 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
-export const CONFIG = {
+window.CONFIG = {
   siteName: "THE FOURWORD",
   googleSheetCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vScwdhjSHbBX7y3_UuR37JdeOsYqQKqU9v_gNVhnsY44PbchbvrSKXARKXagpjB11c--nHYCX6HqV63/pub?gid=0&single=true&output=csv", 
   dictionaryPath: './data/4-letter-words.json',
   fallbackPuzzle: {
     date: "2026-06-03",
     puzzleNumber: 1,
-    seedWord: "GAME" // Using a valid word from your starter dictionary
+    seedWord: "GAME"
   }
 };
