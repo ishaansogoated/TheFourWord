7cb32fe45c1437cf43b1d3da84f41b71f87e7908
ishaansogoated <kyloisverycool@gmail.com>
2026-08-22 23:06:56 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', async () => {
   await loadGameData();
   setupTutorial();
   initializeInteractions();
-  applyAuroTheme();
+
 });
 
 async function getTargetWord(sheetData) {
