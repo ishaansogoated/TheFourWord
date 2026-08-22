8f60cffb003231c9f39cc7b6300f0b451710333c
ishaansogoated <kyloisverycool@gmail.com>
2026-08-22 22:41:48 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,19 @@
 window.CONFIG = {
   siteName: "THE FOURWORD",
-  googleSheetCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vScwdhjSHbBX7y3_UuR37JdeOsYqQKqU9v_gNVhnsY44PbchbvrSKXARKXagpjB11c--nHYCX6HqV63/pub?gid=0&single=true&output=csv", 
+  googleSheetCsvUrl: "https://docs.google.com/spreadsheets/d/1ScwdhjSHbBX7y3_UuR37JdeOsYqQKqU9v_gNVhnsY44P/gviz/tq?tqx=out:csv&sheet=Sheet1",
   dictionaryPath: './data/4-letter-words.json',
+
+  theme: {
+    name: "Sabrina Carpenter",
+    accentPink: "#f8a5c2",
+    vintageCream: "#fff0f5",
+    espressoBrown: "#3d2314",
+    heartIcon: "💋"
+  },
+
   fallbackPuzzle: {
-    date: "2026-06-03",
+    date: "2026-08-05",
     puzzleNumber: 1,
-    seedWord: "GAME"
+    seedWord: "AURO"
   }
 };
